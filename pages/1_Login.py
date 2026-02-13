07324e98a262ce125350b9731f5d86b3fe30a446
Diego <segurad308@gmail.com>
2026-02-13 22:01:44 +0100

    google auth WORKING

--- pages/1_Login.py
+++ pages/1_Login.py
@@ -84,7 +84,7 @@ if "pending_action" not in st.session_state:
     st.session_state.pending_action = None
 
 # Navbar
-login_status = "Logout" if user else "Login"
+login_status = "Profil" if user else "Login"
 st.markdown(f"""  
 <div class="topnav">
 <a href="/" target="_self" class="logo-image-graph logo-link">
@@ -92,10 +92,12 @@ st.markdown(f"""
         class="logo-image-img"
         alt="Tablora Logo" />
 </a>
-            
-<a href="/Offre" target="_self">Offre</a>
+
+<a class="active" href="/Offre" target="_self" >Offre</a>
 <a href="/Creer" target="_self">Créer</a>
-<a class="active" href="/Login" target="_self">{login_status}</a>
+<div class="nav-right">
+    <a class="active" href="/Login" target="_self">{login_status}</a>
+</div>
 </div>
 """, unsafe_allow_html=True)
 
